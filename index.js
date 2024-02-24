329a97af7282c08a7b59fa59378baf991cfe1da7
christian <christiantabakou@gmail.com>
2024-02-25 00:08:26 +0200

    first changes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import store from './store';
+import {Provider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ReduxApp);
