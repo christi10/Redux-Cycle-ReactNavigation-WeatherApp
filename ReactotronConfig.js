94e6c9ed59ada75f92fc759bcfaaaa7f27c0a07e
christian <christiantabakou@gmail.com>
2024-05-19 21:26:42 +0300

    reactotron config

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,10 +2,10 @@ import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { reactotronRedux } from 'reactotron-redux';
 
-const reactotron = Reactotron.configure({ name: 'WeatherApp' }) // Customize the name to your app's name
-    .useReactNative() // Add React Native plugins
+const reactotron = Reactotron.configure({ name: 'WeatherApp' })
+    .useReactNative()
     .use(reactotronRedux())
-    .setAsyncStorageHandler(AsyncStorage) // Set up AsyncStorage
-    .connect(); // Establish a connection to Reactotron
+    .setAsyncStorageHandler(AsyncStorage)
+    .connect();
 
-export default reactotron; // Make it available to the rest of the app
+export default reactotron;
